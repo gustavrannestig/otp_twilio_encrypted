48688817cfb785980dddd0a3fda4ac8bf4d2cf6b
Gustav Rannestig <gustav.rannestig@prototyp.se>
2018-12-19 17:40:06 +0100

    change folder-name again

--- otp_twilio_encrypted/migrations/0001_initial.py
+++ otp_twilio_encrypted/migrations/0001_initial.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
-import otp_twilio.models
+import otp_twilio_encrypted.models
 from django.conf import settings
 
 
@@ -20,7 +20,7 @@ class Migration(migrations.Migration):
                 ('name', models.CharField(help_text='The human-readable name of this device.', max_length=64)),
                 ('confirmed', models.BooleanField(default=True, help_text='Is this device ready for use?')),
                 ('number', models.CharField(help_text='The mobile number to deliver tokens to.', max_length=108)),
-                ('key', models.CharField(default="06cf5e8e255c3a65e90fcd94c5b8e779fad11f71", help_text='A random key used to generate tokens (hex-encoded).', max_length=40, validators=[otp_twilio.models.key_validator])),
+                ('key', models.CharField(default="06cf5e8e255c3a65e90fcd94c5b8e779fad11f71", help_text='A random key used to generate tokens (hex-encoded).', max_length=40, validators=[otp_twilio_encrypted.models.key_validator])),
                 ('user', models.ForeignKey(help_text='The user that this device belongs to.', to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)),
             ],
             options={
